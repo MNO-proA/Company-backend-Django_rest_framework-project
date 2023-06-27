8f4dba12c113d1182e5d954929d23eb957265842
Michael Nana Ofosu <122281966+MNO-proA@users.noreply.github.com>
2023-06-27 11:22:58 +0000

    GET one customer by id api endpoint

--- customers/urls.py
+++ customers/urls.py
@@ -3,5 +3,6 @@ from customers import views
 
 
 urlpatterns = [
-    path('', views.customers, name='customers')
+    path('', views.customers, name='customers'),
+    path('<int:id>', views.customer, name='customer')
 ]
